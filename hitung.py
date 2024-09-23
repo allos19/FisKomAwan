0423a0676c37111565bc6392ad030887696612e0
allos19 <79843182+allos19@users.noreply.github.com>
2024-09-23 10:53:10 +0700

    Update hitung.py

--- hitung.py
+++ hitung.py
@@ -1,21 +1,17 @@
 import streamlit as st
 
 x = st.number_input("Insert a number")
-sx = st.text_input("satuan", "C")
+sx = st.selectbox("Unit", ("C", "K", "F", "R"), key='sx')
 st.write("The current number is ", x,' ',sx)
 
-col1, col2 = st.columns(2)
-
-with col1:
+#col1, col2 = st.columns(2)
+#with col1:
   st.latex(r'''K = C + 273.15''') 
   st.latex(r'''F = \left(\frac{9}{5} \times C \right)''')  
-
-with col2:
+#with col2:
   st.latex(r'''R = \left(\frac{4}{5} \times C \right)''') 
                                                                                                    
- 
-
-sy = st.text_input("Converted to", "C")
+sy = st.selectbox("Converted to", ("C", "K", "F", "R"), key='sy')
 y = 0 
 if (sx == 'C'):
   if (sy == 'C'):
@@ -28,5 +24,41 @@ if (sx == 'C'):
     y = (4/5)*x
   else :
     pass 
+
+if (sx == 'K'):
+	if (sy == 'C'):
+		y = (x-273.15)
+	elif (sy == 'R'):
+		y = ((x-273.15)*4/5)
+	elif (sy=='F'):
+		y = (((x-273.15)*9/5)+32)
+	elif (sy=='K'):
+		y = x
+	else:
+    pass
+
+if (sx == 'F'):
+	if (sy == 'C'):
+		y = ((x-32)*5/9)
+	elif (sy == 'R'):
+		y = ((x-32)*4/9)
+	elif (sy == 'F'):
+		y = x
+	elif (sy == 'K'):
+		y = (((x-32)*5/9)+273.15)
+	else:
+    pass
+
+if (sx == 'R'):
+	if (sy == 'C'):
+		y = (x*5/4)
+	elif (sy == 'R'):
+		y = x
+	elif (sy == 'F'):
+		y = ((x*9/4)+32)
+	elif (sy == 'K'):
+		y = ((x*5/4)+273.15)
+	else:
+    pass
   
 st.write(x, ' ', sx, '=', y, sy)
