c11b09f411e12ff20635427edfb92480de9404d1
allos19 <79843182+allos19@users.noreply.github.com>
2024-09-23 10:16:10 +0700

    Update hitung.py

--- hitung.py
+++ hitung.py
@@ -3,6 +3,19 @@ import streamlit as st
 x = st.number_input("Insert a number")
 sx = st.text_input("satuan", "C")
 st.write("The current number is ", x,' ',sx)
-sy = st.text_input("Converted to", "C")
 
-st.write(x, ' ', sx, '=...', sy)
+st.latex(r'''
+  K = C + 273.15
+  ''') 
+
+sy = st.text_input("Converted to", "C")
+y = 0 
+if (sx == 'C'):
+  if (sy == 'C'):
+    y = x
+  elif (sy == 'K')
+    y = x+273.15
+  else 
+    pass 
+  
+st.write(x, ' ', sx, '=', y, sy)
