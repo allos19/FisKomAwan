51d27a9c2205a8003c13d8bf6da6ea1b17cdf877
allos19 <79843182+allos19@users.noreply.github.com>
2024-09-23 10:00:08 +0700

    Update hitung.py

--- hitung.py
+++ hitung.py
@@ -3,3 +3,6 @@ import streamlit as st
 x = st.number_input("Insert a number")
 sx = st.text_input("satuan", "C")
 st.write("The current number is ", x,' ',sx)
+sy = st.text_input("Converted to", "K")
+
+st.writer = (x, ' ', sx, '=...', sy)
