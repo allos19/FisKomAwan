845a0700caf5be75e80a4947ab53e52fb7252a1d
allos19 <79843182+allos19@users.noreply.github.com>
2024-09-23 10:39:25 +0700

    Update hitung.py

--- hitung.py
+++ hitung.py
@@ -4,13 +4,11 @@ x = st.number_input("Insert a number")
 sx = st.text_input("satuan", "C")
 st.write("The current number is ", x,' ',sx)
 
+col1, col2 = st.columns(2)
 
-st.latex(r'''
-  K = C + 273.15
-  ''') 
-st.latex(r'''
-  F = \left(\frac{9}{5} \times C \right)
-  ''')  
+with col1:
+  st.latex(r'''K = C + 273.15''') 
+  st.latex(r'''F = \left(\frac{9}{5} \times C \right)''')  
  
 
 sy = st.text_input("Converted to", "C")
